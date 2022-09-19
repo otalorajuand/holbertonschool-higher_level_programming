da27ab174954098c0dfad73f39b4f56b7c46cb41
otalorajuand <otalorajuand@gmail.com>
2022-09-19 23:58:14 +0000

    addition of task 6

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+for i in range(10):
+    
+    j = i + 1
+    while  j < 10:
+        if (i != 0 or j != 1):
+            print(", ", end = "")
+        print("{}{}".format(i, j), end = "")
+        j += 1
