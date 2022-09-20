2dedeb5818e233ceb1d87c5331a0865c4c733c96
otalorajuand <otalorajuand@gmail.com>
2022-09-20 21:36:21 +0000

    Addition of task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,13 +4,13 @@ import sys
 if __name__ == "__main__":
 
     lenght = len(sys.argv) - 1
-    
+
     if lenght > 1:
         print("{} arguments:".format(lenght))
     elif lenght == 1:
         print("{} argument:".format(lenght))
     else:
-        print("{} argument.".format(lenght))
+        print("{} arguments.".format(lenght))
 
     for i in range(1, lenght + 1):
         print("{}: {}".format(i, sys.argv[i]))
