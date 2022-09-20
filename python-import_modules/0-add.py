b66d46fee55bd2146a440baa016fb9416cc5c44b
otalorajuand <otalorajuand@gmail.com>
2022-09-20 20:54:57 +0000

    Addition of task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import add_0
 
-a = 1
-b = 2 
+if __name__ == "__main__":
 
-print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+    a = 1
+    b = 2 
+
+    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
