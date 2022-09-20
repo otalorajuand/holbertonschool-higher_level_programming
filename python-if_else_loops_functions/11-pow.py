676649cc0b70676e732d7c304fd901739068d131
otalorajuand <otalorajuand@gmail.com>
2022-09-20 15:24:35 +0000

    addition of task 11

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# pow - computes a to the power of b.
+# @a: The first integer.
+# @b: The second integer.
+# Return: The value of the operation.
+
+
+def pow(a, b):
+    return (a ** b)
