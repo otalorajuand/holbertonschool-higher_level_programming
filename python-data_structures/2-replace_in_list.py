30deb50ae68433fecc832457ceca39d3410d789e
otalorajuand <otalorajuand@gmail.com>
2022-09-20 22:51:35 +0000

    Addition of task 2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -9,9 +9,7 @@ def replace_in_list(my_list, idx, element):
         Return: The new list.
     """
 
-    if idx < 0 or idx >= len(my_list):
-        return None
-
-    my_list[idx] = element
+    if not (idx < 0 or idx >= len(my_list)):
+        my_list[idx] = element
 
     return my_list
