97b813c365a3e8765e21f5718fd092e27425285e
otalorajuand <otalorajuand@gmail.com>
2022-09-21 17:33:42 +0000

    addition of task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,10 +3,11 @@
 
 def max_integer(my_list=[]):
 
-    max = 0
 
-    if my_list == [[]]:
+    if my_list == []:
         return None
+
+    max = my_list[0]
 
     for elem in my_list:
         if elem > max:
