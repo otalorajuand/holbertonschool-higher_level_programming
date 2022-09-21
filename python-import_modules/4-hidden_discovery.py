2d996497993a15ed59b140ee0ccf93e7a500abbf
otalorajuand <otalorajuand@gmail.com>
2022-09-21 20:35:53 +0000

    addition of task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3                                                                                                                              
-import hidden_4                                                                                                                                 
-                                                                                                                                                
-lista = dir(hidden_4)                                                                                                                           
-                                                                                                                                                
-for elem in lista:                                                                                                                              
-        if elem[0] != "_":                                                                                                                      
-                print("{}".format(elem)) 
+#!/usr/bin/python3
+import hidden_4
 
+if __name__ == "__main__":
+
+    lista = dir(hidden_4)
+
+    for elem in lista:
+        if elem[0] != "_":
+            print("{}".format(elem))
