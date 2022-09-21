ddfd5438f0a09e45b77bb8bbd63128ff5ac0f7c5
otalorajuand <otalorajuand@gmail.com>
2022-09-21 18:57:45 +0000

    Addition of task 10

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def divisible_by_2(my_list=[]):
+
+    list_result = []
+
+    for elem in my_list:
+        if elem % 2 == 0:
+            list_result.append(True)
+        else:
+             list_result.append(False)
+
+    return list_result
