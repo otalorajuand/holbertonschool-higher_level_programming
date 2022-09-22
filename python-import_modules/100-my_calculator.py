1a53c5f58d3ab3fdc5073a61249d461d2a910c18
otalorajuand <otalorajuand@gmail.com>
2022-09-22 00:25:31 +0000

    addition of task 100

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,23 +2,24 @@
 from calculator_1 import add, sub, mul, div
 import sys
 
-if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
 
-num_1 = sys.argv[1]
-num_2 = sys.argv[3]
-operator = sys.argv[2]
+    num_1 = sys.argv[1]
+    num_2 = sys.argv[3]
+    operator = sys.argv[2]
 
-match operator:
-    case '+':
-        print('{} + {} = {}'.format(num_1, num_2, add(num_1, num_2)))
-    case '-':
-        print('{} - {} = {}'.format(num_1, num_2, sub(num_1, num_2)))
-    case '*':
-        print('{} * {} = {}'.format(num_1, num_2, mul(num_1, num_2)))
-    case '/':
-        print('{} / {} = {}'.format(num_1, num_2, div(num_1, num_2)))
-    case other:
-        print('Unknown operator. Available operators: +, -, * and /')
+    match operator:
+        case '+':
+            print('{} + {} = {}'.format(num_1, num_2, add(num_1, num_2)))
+        case '-':
+            print('{} - {} = {}'.format(num_1, num_2, sub(num_1, num_2)))
+        case '*':
+            print('{} * {} = {}'.format(num_1, num_2, mul(num_1, num_2)))
+        case '/':
+            print('{} / {} = {}'.format(num_1, num_2, div(num_1, num_2)))
+        case other:
+            print('Unknown operator. Available operators: +, -, * and /')
 
