34e945132b50304e95072efbae7f3eff5d2cc753
otalorajuand <otalorajuand@gmail.com>
2022-09-22 00:23:58 +0000

    addition of task 100

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+
+if len(sys.argv) != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    exit(1)
+
+num_1 = sys.argv[1]
+num_2 = sys.argv[3]
+operator = sys.argv[2]
+
+match operator:
+    case '+':
+        print('{} + {} = {}'.format(num_1, num_2, add(num_1, num_2)))
+    case '-':
+        print('{} - {} = {}'.format(num_1, num_2, sub(num_1, num_2)))
+    case '*':
+        print('{} * {} = {}'.format(num_1, num_2, mul(num_1, num_2)))
+    case '/':
+        print('{} / {} = {}'.format(num_1, num_2, div(num_1, num_2)))
+    case other:
+        print('Unknown operator. Available operators: +, -, * and /')
+
