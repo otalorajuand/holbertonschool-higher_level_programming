e04e7308df63e95f61e2ee1ce0e31de1afe08a50
otalorajuand <otalorajuand@gmail.com>
2022-09-26 19:10:00 +0000

    addition of task 2

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -4,4 +4,4 @@
 def multiply_by_2(a_dictionary):
 
     """returns a new dictionary with all values multiplied by 2"""
-    return ({key: value ** 2 for (key, value) in a_dictionary.items()})
+    return ({key: value * 2 for (key, value) in a_dictionary.items()})
