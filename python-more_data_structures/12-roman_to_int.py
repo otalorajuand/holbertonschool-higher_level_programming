98ee26e31ccc4ff5d8ed5a973992437f049f0ab1
otalorajuand <otalorajuand@gmail.com>
2022-09-26 21:22:09 +0000

    addition of task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -9,7 +9,7 @@ def roman_to_int(roman_string):
 
     number = 0
     last_entry = 1000
-    symbols = dict(I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000)
+    symbols = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
 
     for letter in roman_string:
         digit = symbols[letter]
