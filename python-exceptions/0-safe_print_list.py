e4f1a60a678f6d84d77cf9f07c6bfd2fa75485a8
otalorajuand <otalorajuand@gmail.com>
2022-09-27 14:44:57 +0000

    addition of task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,10 +3,13 @@
 
 def safe_print_list(my_list=[], x=0):
 
-    try:
-        for i in range(x + 1):
-            print(my_list[i], end="")
-    except IndexError:
-        pass
+    """prints x elements of a list."""
+    i = 0
+    while i <= x:
+        try:
+             print(my_list[i], end="")
+             i += 1
+        except IndexError:
+            break
     print()
-    return (i)
+    return (i) 
