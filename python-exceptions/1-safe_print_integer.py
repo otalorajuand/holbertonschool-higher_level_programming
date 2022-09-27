4c035ab9f1d07f655f6da0330338108486df7247
otalorajuand <otalorajuand@gmail.com>
2022-09-27 14:59:42 +0000

    addition of task 1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -6,7 +6,7 @@ def safe_print_integer(value):
     """prints an integer with "{:d}".format()."""
     try:
         print("{:d}".format(value))
-    except ValueError:
+    except (ValueError, TypeError):
         return (False)
 
     return (True)
