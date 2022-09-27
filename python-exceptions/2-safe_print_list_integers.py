bda542bbea95718e6a9d35bfddb3053a5c38f780
otalorajuand <otalorajuand@gmail.com>
2022-09-27 15:11:32 +0000

    addition of task 2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+
+    """prints the first x elements of a list and only integers."""
+    i = 0
+    counter = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            counter += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return (counter)
