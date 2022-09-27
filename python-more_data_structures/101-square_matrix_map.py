c24bacde47d7e95f70b7677150e4ec4d24cfb2b0
otalorajuand <otalorajuand@gmail.com>
2022-09-27 18:13:00 +0000

    addition of task 101

---
+++ python-more_data_structures/101-square_matrix_map.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def square_matrix_simple(matrix=[]):
+
+    """computes the square value of all integers of a matrix."""
+    res = [list(map(lambda x: x ** 2, row)) for row in matrix]
+
+    return (res)
