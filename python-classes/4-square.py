f1c35a606810608610c84d50165db6ec718b5523
otalorajuand <otalorajuand@gmail.com>
2022-09-28 18:23:11 +0000

    addition of task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    """defines a square
+
+        Attributes:
+        size (int): The size of a side of the square.
+    """
+    def __init__(self, size=0):
+        """initializes the object"""
+        self.size = size
+
+    @property
+    def size(self):
+        """object getter"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """object setter"""
+        if (not isinstance(value, int)):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """calculates the area of the square"""
+        return (self.__size ** 2)
