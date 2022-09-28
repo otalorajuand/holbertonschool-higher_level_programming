ae28d1536e0179aa9c1ec9f27010804101ff19cf
otalorajuand <otalorajuand@gmail.com>
2022-09-28 18:57:46 +0000

    Addition of task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -8,18 +8,19 @@ class Square:
         Attributes:
         size (int): The size of a side of the square.
     """
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """initializes the object"""
         self.size = size
+        self.position = position
 
     @property
     def size(self):
-        """object getter"""
+        """object size getter"""
         return (self.__size)
 
     @size.setter
     def size(self, value):
-        """object setter"""
+        """object size setter"""
         if (not isinstance(value, int)):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -27,11 +28,26 @@ class Square:
         else:
             self.__size = value
 
+    @property
+    def position(self):
+        """object position getter"""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        """object position setter"""
+        if (not isinstance(value, tuple) or value[0] < 0 or value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
     def area(self):
         """calculates the area of the square"""
         return (self.__size ** 2)
 
     def my_print(self):
         """prints in stdout the square with the character #"""
+
         for i in range(self.size):
+            print(" " * self.position[0], end="")
             print("#" * self.size)
