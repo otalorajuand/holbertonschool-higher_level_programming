4b7178adf509cde2c634c51ad22e60ffa48b7791
otalorajuand <otalorajuand@gmail.com>
2022-09-28 18:07:42 +0000

    addition of task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    """defines a square
+        
+        Attributes:
+        size (int): The size of a side of the square.
+    """
+    def __init__(self, size=0):
+        if (not isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
