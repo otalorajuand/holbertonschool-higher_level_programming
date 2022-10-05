371713fbf116959dcd6db6fde11e14fa38e8d5a0
otalorajuand <otalorajuand@gmail.com>
2022-10-05 00:02:47 +0000

    Addition of task 3

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This is the "3-say_my_name" module.
+The example module supplies one function, say_my_name().  For example,
+
+>>> say_my_name("John", "Smith")
+My name is John Smith"""
+
+
+def say_my_name(first_name="", last_name=""):
+     """prints My name is <first name> <last name>
+    First and Last names must be strings."""
+
+    if type(first_name) is not str:
+        raise TypeError('first_name must be a string')
+
+    if type(last_name) is not str:
+        raise TypeError('last_name must be a string')
+
+    print(f"My name is {first_name} {last_name}")
