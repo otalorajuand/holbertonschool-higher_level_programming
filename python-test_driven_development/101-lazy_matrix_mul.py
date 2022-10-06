76cf9b3dbc1db1e39b2297994f8647e610f9d5ac
otalorajuand <otalorajuand@gmail.com>
2022-10-06 15:43:28 +0000

    addition of task 101

---
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import numpy as np
+"""This is the "101-lazy_matrix-mul" module.
+The example module supplies one function, lazy_matrix_mul().  For example,
+>>> lazy_matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]])
+[[7, 10], [15, 22]]
+"""
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Multiplies two matrices.
+
+    Attributes:
+    m_a: first matrix
+    m_b: second matrix
+    Returns: The resulting matrix."""
+
+    if type(m_a) is not list:
+        raise TypeError('m_a must be a list')
+
+    if type(m_b) is not list:
+        raise TypeError('m_b must be a list')
+
+    if m_a == [] or m_a == [[]]:
+        raise ValueError(r"m_a can't be empty")
+
+    if m_b == [] or m_b == [[]]:
+        raise ValueError(r"m_b can't be empty")
+
+    for row in m_a:
+        if type(row) is not list:
+            raise TypeError('m_a must be a list of lists')
+        if len(row) != len(m_a[0]):
+            raise TypeError('each row of m_a must be of the same size')
+        for elem in row:
+            if type(elem) not in [int, float]:
+                raise TypeError('m_a should contain only integers or floats')
+
+    for row in m_b:
+        if type(row) is not list:
+            raise TypeError('m_b must be a list of lists')
+        if len(row) != len(m_b[0]):
+            raise TypeError('each row of m_b must be of the same size')
+        for elem in row:
+            if type(elem) not in [int, float]:
+                raise TypeError('m_b should contain only integers or floats')
+
+    if len(m_a[0]) != len(m_b):
+        raise ValueError(r"m_a and m_b can't be multiplied")
+
+    a = np.array(m_a)
+    b = np.array(m_b)
+
+    return (a @ b)
