77b6462e7eccc8306fbf97d65ad3a22011138e7d
otalorajuand <otalorajuand@gmail.com>
2022-10-06 23:19:57 +0000

    Addition of task 101

--- python-more_classes/101-nqueens.py
+++ python-more_classes/101-nqueens.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import sys
 
+
 def NQueens(n):
-    
+
     col = set()
     posDiag = set()
     negDiag = set()
@@ -15,18 +16,18 @@ def NQueens(n):
             copy = [row for row in board]
             res.append(copy)
             return
-            
+
         for c in range(n):
             if c in col or (r + c) in posDiag or (r - c) in negDiag:
                 continue
-                
+
             col.add(c)
             posDiag.add(r + c)
             negDiag.add(r - c)
             board.append([r, c])
 
             backtrack(r + 1)
-                
+
             col.remove(c)
             posDiag.remove(r + c)
             negDiag.remove(r - c)
@@ -34,5 +35,22 @@ def NQueens(n):
     backtrack(0)
     return res
 
-for elem in NQueens(int(sys.argv[1])):
-    print(elem)
+
+def main():
+
+    if len(sys.argv) != 2:
+        print('Usage: nqueens N')
+        exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print('N must be a number')
+        exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    for elem in NQueens(n):
+        print(elem)
