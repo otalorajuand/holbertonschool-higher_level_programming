e152fc18ae3918b699a5c5749f5dd0df76f83e52
otalorajuand <otalorajuand@gmail.com>
2022-10-11 23:04:05 +0000

    Addition of task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -13,6 +13,6 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         if BaseGeometry.integer_validator(self, "width", width):
             self.__width = width
-        
+
         if BaseGeometry.integer_validator(self, "height", height):
             self.__height = height
