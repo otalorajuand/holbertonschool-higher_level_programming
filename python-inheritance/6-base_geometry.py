d18816ecaf87e008516c3497d9689be53eeddf40
otalorajuand <otalorajuand@gmail.com>
2022-10-11 20:27:20 +0000

    addition of task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains the class BaseGeomtry
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry Class"""
+    
+    def area(self):
+        raise Exception('area() is not implemented')
