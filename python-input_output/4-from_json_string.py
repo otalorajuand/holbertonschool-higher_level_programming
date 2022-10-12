cac4a93e14ba7a912102c53c7b042cc8bb73c6a4
otalorajuand <otalorajuand@gmail.com>
2022-10-12 18:45:12 +0000

    Addition of task 4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module contains the function from_json_string()
+"""
+import json
+
+def from_json_string(my_str):
+    """Returns the python represention of the json string
+
+    Attributes:
+        my_str (str): The string containing the json to convert."""
+    return (json.loads(my_str))
