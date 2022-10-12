7b6393df236c225898a4a07c96048c09f626f27d
otalorajuand <otalorajuand@gmail.com>
2022-10-12 18:40:14 +0000

    Addition of task 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains the function to_json_string()
+"""
+import json
+
+def to_json_string(my_obj):
+    """Returns the json represention of the object
+    Attributes:
+        my_obj (object): The object to convert."""
+    return (json.dumps(my_obj))
