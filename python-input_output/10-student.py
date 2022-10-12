9e68a6f3dcd0f64725e0761e2ada270b5ad8c050
otalorajuand <otalorajuand@gmail.com>
2022-10-12 22:12:53 +0000

    Addition of task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module contains the class Students
+"""
+
+
+class Student:
+    """This class defines a student
+    Attributes:
+        first_name (str): The first name of the student
+        last_name (str): The last name of the student
+        age (int): The age of the student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        original_attrs = self.__dict__
+
+        if not attrs:
+            return (original_attrs)
+        return ({key: value for key, value in original_attrs.items()
+                if key in attrs})
