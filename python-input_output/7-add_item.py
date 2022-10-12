ead9cbd0a7a7021c71d9604e3233029b69aeebfb
otalorajuand <otalorajuand@gmail.com>
2022-10-12 20:35:09 +0000

    Addition of task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,8 +7,8 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 json_file_name = "add_item.json"
-   
-try: 
+
+try:
     lista = load_from_json_file(json_file_name)
 except FileNotFoundError:
     lista = []
