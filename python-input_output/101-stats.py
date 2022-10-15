46c8f392a656a0be4a739cdb54224d69da8939c5
otalorajuand <otalorajuand@gmail.com>
2022-10-15 00:43:31 +0000

    Addition of task 101

---
+++ python-input_output/101-stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import sys
+import signal
+
+
+def main():
+    acum = 0
+    num_lines = 0
+    dict_lines = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+                "404": 0, "405": 0, "500": 0}
+
+    for line in sys.stdin:
+
+        striped_line = line.rstrip().split(" ")
+        dict_lines[striped_line[7]] += 1
+
+        
+        if (num_lines % 10 == 0 and num_lines != 0):
+            print(f"File size: {acum}")
+            for key in sorted(dict_lines):
+                print(f"{key}: {dict_lines[key]}")
+
+        acum += int(striped_line[8])
+        num_lines += 1
+
+
+if __name__ == "__main__": 
+    main()
+
