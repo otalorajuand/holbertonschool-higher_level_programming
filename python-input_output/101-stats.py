cd70f595ead2f0320f2a5a6a6558da7a7a6ff683
otalorajuand <otalorajuand@gmail.com>
2022-10-15 01:04:29 +0000

    Addition of task 101

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -13,28 +13,26 @@ def main():
     acum = 0
     num_lines = 1
     dict_lines = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-                "404": 0, "405": 0, "500": 0}
+                            "404": 0, "405": 0, "500": 0}
 
     for line in sys.stdin:
 
         striped_line = line.rstrip().split(" ")
         dict_lines[striped_line[7]] += 1
+        acum += int(striped_line[8])
 
-        
         if (num_lines % 10 == 0):
             print(f"File size: {acum}")
             for key in sorted(dict_lines):
                 if dict_lines[key]:
                     print(f"{key}: {dict_lines[key]}")
 
-        acum += int(striped_line[8])
         num_lines += 1
 
     print(f"File size: {acum}")
     for key in sorted(dict_lines):
         if dict_lines[key]:
             print(f"{key}: {dict_lines[key]}")
-
 
 
 if __name__ == "__main__":
