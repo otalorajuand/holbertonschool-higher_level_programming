0b09f55371fcd518cbaa49e190e87827170a87a9
otalorajuand <otalorajuand@gmail.com>
2022-10-19 20:21:36 +0000

    Addition of tests

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -144,6 +144,13 @@ class TestRectangle(unittest.TestCase):
 
         self.assertEqual(reading, "[]")
 
+        Rectangle.save_to_file([])
+
+        with open("Rectangle.json", "r") as file:
+            reading = file.read()
+
+        self.assertEqual(reading, "[]")
+
         r1 = Rectangle(10, 7, 2, 8)
         r2 = Rectangle(2, 4)
         Rectangle.save_to_file([r1, r2])
