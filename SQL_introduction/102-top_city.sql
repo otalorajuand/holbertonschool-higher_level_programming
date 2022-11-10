f3c2a22115bb4f6e5d260307f7de5fb27293c6c1
otalorajuand <otalorajuand@gmail.com>
2022-11-10 22:10:18 +0000

    Addition of task 102

--- SQL_introduction/102-top_city.sql
+++ SQL_introduction/102-top_city.sql
@@ -1,3 +1,5 @@
+-- displays the top 3 of cities temperature during July and August
+-- ordered by temperature (descending)
 SELECT city, avg(value) AS avg_temp
 FROM temperatures
 WHERE MONTH BETWEEN 7 AND 8
