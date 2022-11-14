55ff0703f00d99e055bb6e775bf7a2e387fadd61
otalorajuand <otalorajuand@gmail.com>
2022-11-14 15:09:34 +0000

    Addition of task 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- creates the database hbtn_0d_usa and the table states
+-- (in the database hbtn_0d_usa) on your MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
+(id INT DEFAULT 1 UNIQUE AUTO_INCREMENT PRIMARY KEY,
+ name VARCHAR(256));
+ 
