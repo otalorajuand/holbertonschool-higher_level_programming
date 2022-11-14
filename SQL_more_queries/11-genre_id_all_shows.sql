45a671274bae58911d80a1d03b6f6068d0f766a5
otalorajuand <otalorajuand@gmail.com>
2022-11-14 18:41:33 +0000

    Addition of task 11

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- lists all shows contained in the database hbtn_0d_tvshows.
+SELECT t1.title, t2.genre_id
+FROM tv_shows t1
+LEFT JOIN  tv_show_genres t2
+ON t1.id = t2.show_id
+ORDER BY t1.title ASC, t2.genre_id ASC;
