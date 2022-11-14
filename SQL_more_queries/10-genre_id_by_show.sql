7936e0708d846ecf92c96047035f47ab41c845b0
otalorajuand <otalorajuand@gmail.com>
2022-11-14 18:32:17 +0000

    Addition of task 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,7 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least
 -- one genre linked.
-SELECT t1.title, t2.genre_id
-FROM tv_shows t1
-LEFT JOIN tv_show_genres t2
-ON t1.id = t2.genre_id
-ORDER BY t1.title ASC, t2.genre_id ASC;
+SELECT t2.title, t1.genre_id
+FROM tv_show_genres t1
+LEFT JOIN tv_shows t2
+ON t1.genre_id = t2.id
+ORDER BY t2.title ASC, t1.genre_id ASC;
