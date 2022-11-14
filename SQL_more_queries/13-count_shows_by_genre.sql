dc597edb04761076f8a96e0039b27fd43ace4b07
otalorajuand <otalorajuand@gmail.com>
2022-11-14 19:23:49 +0000

    Addition of task 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,10 +1,10 @@
 --  lists all genres from hbtn_0d_tvshows and displays the number
 -- of shows linked to each.
-SELECT t3.name, count(*) as number_of_shows
+SELECT t3.name, count(*) AS number_of_shows
 FROM tv_shows t1
 LEFT JOIN  tv_show_genres t2
 ON t1.id = t2.show_id
 LEFT JOIN tv_genres t3
 ON t2.genre_id = t3.id
-WHERE t2.genre_id IS not NULL
+WHERE t2.genre_id IS NOT NULL
 GROUP BY t3.name;
