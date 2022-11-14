c3cd37c27881c5c812d31f09f56ef33fd8601dec
otalorajuand <otalorajuand@gmail.com>
2022-11-14 18:19:32 +0000

    Addition of task 10

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- lists all shows contained in hbtn_0d_tvshows that have at least
+-- one genre linked.
+SELECT t1.title, t2.genre_id
+FROM tv_shows t1
+LEFT JOIN tv_show_genres t2
+ON t1.id = t2.genre_id
+ORDER BY t1.titles AND t2.genre_id;
