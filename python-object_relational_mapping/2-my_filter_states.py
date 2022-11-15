19bc506de8fe552fd8de9e0ccacc91c000877606
otalorajuand <otalorajuand@gmail.com>
2022-11-15 18:09:08 +0000

    Addition of task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''
+This module lists all states from the database hbtn_0e_0_usa
+sorted in ascending order by states.id
+'''
+import MySQLdb
+from sys import argv
+
+
+def my_filter_states():
+    """This function connects to the database and lists all states
+       from the database hbtn_0e_0_usa
+       sorted in ascending order by states.id"""
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3])
+
+    cur = db.cursor()
+    query = "SELECT id, name FROM states WHERE name = %s \
+             ORDER BY states.id ASC"
+    cur.execute(query, [argv[4]])
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    db.close()
+
+
+if __name__ == "__main__":
+    my_filter_states()
