33c186b9fae3c19c5fa4dbb90e2c9fc8f7845eb3
otalorajuand <otalorajuand@gmail.com>
2022-11-16 00:47:49 +0000

    Addition of task 9

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module contains the function model_state_fetch_first()
+This module contains the function model_state_filter_a()
 """
 from model_state import Base, State
 import sys
@@ -8,8 +8,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-def model_state_fetch_first():
-    """prints the first State object from the database hbtn_0e_6_usa"""
+def model_state_filter_a():
+    """lists all State objects that contain the letter a from the database hbtn_0e_6_usa"""
 
     url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
                                                       sys.argv[2],
@@ -23,13 +23,12 @@ def model_state_fetch_first():
     conn = engine.connect()
     session = Session(bind=conn)
 
-    objs = session.query(State).order_by(State.id).first()
+    objs = session.query(State).order_by(State.id).all()
 
-    if objs:
-        print("{}: {}".format(objs.id, objs.name))
-    else:
-        print('Nothing')
+    for obj in objs:
+        if 'a' in obj.name:
+            print("{}: {}".format(obj.id, obj.name))
 
 
 if __name__ == "__main__":
-    model_state_fetch_first()
+    model_state_filter_a()
