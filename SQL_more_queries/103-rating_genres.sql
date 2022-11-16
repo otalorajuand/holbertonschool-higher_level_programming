0617bef86cd841736662d58734df874b91d518d7
otalorajuand <otalorajuand@gmail.com>
2022-11-16 18:20:10 +0000

    Addition of task 103

---
+++ SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,9 @@
+-- lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+SELECT t1.name, sum(t3.rate) as rating
+FROM tv_genres t1
+LEFT JOIN tv_show_genres t2
+ON t1.id = t2.genre_id
+LEFT JOIN tv_show_ratings t3
+ON t2.show_id = t3.show_id
+GROUP BY t1.name
+ORDER BY sum(t3.rate) DESC;
