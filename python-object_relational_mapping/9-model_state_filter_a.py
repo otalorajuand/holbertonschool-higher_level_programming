379cb0b3d64d52833ae7e06b59b8677f93557375
otalorajuand <otalorajuand@gmail.com>
2022-11-16 00:48:59 +0000

    Addition of task 9

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -9,7 +9,8 @@ from sqlalchemy.orm import sessionmaker
 
 
 def model_state_filter_a():
-    """lists all State objects that contain the letter a from the database hbtn_0e_6_usa"""
+    """lists all State objects that contain the letter a
+       from the database hbtn_0e_6_usa"""
 
     url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
                                                       sys.argv[2],
