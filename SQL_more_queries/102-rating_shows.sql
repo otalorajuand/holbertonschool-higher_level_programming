8bf22945876ed00bfc95173f9361c1e2b971c645
otalorajuand <otalorajuand@gmail.com>
2022-11-16 18:13:37 +0000

    Addition of task 102

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
 -- lists all shows from hbtn_0d_tvshows_rate by their rating.
-SELECT t1.title, sum(t2.rate) as rating
+SELECT t1.title, sum(t2.rate) AS rating
 FROM tv_shows t1
 LEFT JOIN tv_show_ratings t2
 ON t1.id = t2.show_id
