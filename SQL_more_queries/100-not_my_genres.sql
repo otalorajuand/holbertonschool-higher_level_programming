b9d6eec5e49a979dc54833ba4ca7595be6210fad
otalorajuand <otalorajuand@gmail.com>
2022-11-16 17:14:40 +0000

    Addition of task 100

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -1,9 +1,9 @@
 -- list all genres not linked to the show Dexter
-SELECT t1.title, t3.name
+SELECT t3.name
 FROM tv_shows t1
 LEFT JOIN  tv_show_genres t2
 ON t1.id = t2.show_id
 LEFT JOIN tv_genres t3
 ON t2.genre_id = t3.id
-WHERE t1.title IS NOT 'Dexter'
-ORDER BY t1.title ASC, t3.name ASC;
+WHERE t1.title <> "Dexter"
+ORDER BY t3.name ASC;
