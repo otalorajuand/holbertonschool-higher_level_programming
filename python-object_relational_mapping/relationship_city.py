8d3dd407b8be0e78868c1b779a4217536e6c4bed
otalorajuand <otalorajuand@gmail.com>
2022-11-17 18:43:36 +0000

    Addition of task 100

--- python-object_relational_mapping/relationship_city.py
+++ python-object_relational_mapping/relationship_city.py
@@ -7,8 +7,8 @@ from relationship_state import Base
 
 
 class City(Base):
-    from relationship_state import State
     """This class represents the table City"""
+    from relationship_state import State
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
