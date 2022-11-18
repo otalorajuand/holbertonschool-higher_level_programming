b0618a0b9d7f4643d8239b50b9c651a929d699f9
otalorajuand <otalorajuand@gmail.com>
2022-11-18 02:01:31 +0000

    Addition of task 101

---
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This module contains the function relationship_states_cities()
+"""
+from relationship_state import Base, State
+from relationship_city import City
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def relationship_states_cities():
+    """adds the State object “Louisiana” to the database hbtn_0e_6_usa"""
+
+    url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                      sys.argv[2],
+                                                      sys.argv[3])
+
+    engine = create_engine(url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    State.metadata.create_all(engine)
+    City.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
+    conn = engine.connect()
+    session = Session(bind=conn)
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("\t{}: {}".format(city.id, city.name))
+
+
+if __name__ == "__main__":
+    relationship_states_cities()
