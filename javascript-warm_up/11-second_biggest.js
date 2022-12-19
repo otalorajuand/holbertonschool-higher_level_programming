5d7a5d9a2b6f4d1006ce08534d3d1b5a38858071
otalorajuand <otalorajuand@gmail.com>
2022-12-19 23:12:06 +0000

    Addition of task 11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -6,7 +6,7 @@ let arrSorted;
 for (let i = 2; i < process.argv.length; i++) {
   arr[i] = parseInt(process.argv[i]);
 }
-arrSorted = arr.sort();
+arrSorted = arr.sort(function (a, b) { return a - b; });
 arrSorted = arrSorted.reverse();
 
 console.log(arrSorted[1]);
