80497da157083501e8fa5ae9c9e69fe953b797fa
Otalorajuand <otalorajuand@gmail.com>
2022-12-24 18:13:48 -0500

    Addition of task 102

--- javascript_objects_scopes_closures/101-sorted.js
+++ javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
-
 const newDict = {};
 for (const key in dict) {
-  if (newDict[dict[key]]) {
+  if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [];
-  } else {
-    newDict[dict[key]].push(key);
   }
+  newDict[dict[key]].push(key);
 }
+console.log(newDict);
